f150888bc9203154942ebdf194ecc97d296d60f4
Pavan Kumar Merugu <pavan@Pavans-MacBook-Pro.local>
2020-08-06 13:57:18 +0200

    initial commit

---
+++ dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.6
+
+# Create app directory
+WORKDIR /app
+
+# Install app dependencies
+#COPY src/requirements.txt ./
+
+ADD . /app
+
+RUN pip install -r requirements.txt
+
+# Bundle app source
+#COPY src /app
+
+EXPOSE 5000
+CMD [ "python", "final.py" ]
---
+++ final.py
@@ -0,0 +1,113 @@
+from flask import render_template
+from flask import Flask, flash, request, redirect
+import paramiko
+import re
+
+
+#p = paramiko.SSHClient()
+#p.set_missing_host_key_policy(
+#    paramiko.AutoAddPolicy())  # This script doesn't work for me unless this line is added!
+#p.connect("172.23.254.117", port=22, username="testuser1", password="123Omni:us")
+
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+message = ""
+flag = 0
+@app.route('/')
+
+def home():
+    return render_template('password.html')
+
+@app.route('/password', methods=['POST', 'GET'])
+def password():
+    if request.method == 'POST':
+        user = request.form['uname']
+        oldpassword = request.form['oldpassword']
+        newpassword = request.form['newpassword']
+        confirmnewpassword = request.form['confirmnewpassword']
+        #if user == 'pavan':
+        #    return render_template('changepassword.html')
+        #else:
+        #    return 'you failed'
+
+
+
+        #oldpassword = input("Enter old password:")
+        #newpassword = input("Enter new password:")
+
+        # stdin, stdout, stderr = p.exec_command('ipconfig')
+        if newpassword == confirmnewpassword:
+            print('Password matches')
+
+            while True:
+                if (len(newpassword) < 8):
+                    flag = -1
+                    break
+                elif not re.search("[a-z]", newpassword):
+                    flag = -1
+                    break
+                elif not re.search("[A-Z]", newpassword):
+                    flag = -1
+                    break
+                elif not re.search("[0-9]", newpassword):
+                    flag = -1
+                    break
+                elif not re.search("[!&/?%:_@$]", newpassword):
+                    flag = -1
+                    break
+                elif re.search("\s", newpassword):
+                    flag = -1
+                    break
+                else:
+                    flag = 0
+                    print("Valid new Password")
+
+                    try:
+                        p = paramiko.SSHClient()
+                        p.set_missing_host_key_policy(
+                            paramiko.AutoAddPolicy())  # This script doesn't work for me unless this line is added!
+                        p.connect("172.23.254.117", port=22, username="testuser1", password="123Omni@us")
+                        print('connected')
+                    except paramiko.AuthenticationException:
+                        print('Failed to connect to %s due to wrong username/password')
+                        exit(1)
+                    except Exception as e:
+                        print(e.message)
+                        exit(2)
+
+
+                    stdin, stdout, stderr = p.exec_command("Set-AdAccountPassword -Identity " + user + " -OldPassword "
+                                            "(ConvertTo-SecureString -AsPlainText"
+                                            " " + oldpassword + " -Force) -NewPassword (ConvertTo-SecureString "
+                                            "-AsPlainText " + newpassword + " -Force)")
+
+                    err = stderr.readlines()
+                    opt = stdout.readlines()
+                    err = "".join(err)
+                    opt = "".join(opt)
+                    print(err)
+                    print(user)
+                    p.close()
+                    break
+                    #global message
+            message = "Password changed successfully"
+
+            if flag == -1:
+                print("Not a Valid Password")
+                message = "not a valid password.check the password policy mentioned here"
+        else:
+            print('old and new passwords do not match')
+            #global message
+            message = "passwords do not match"
+
+    print(message)
+    flash(message)
+    return render_template('password.html')
+
+    #return render_template('userform.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
